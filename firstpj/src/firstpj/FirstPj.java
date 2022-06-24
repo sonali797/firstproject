59b9108a0e85a2fa78ed822654b4e28e6247952b
sonali p <sonali p@DESKTOP-8JCBGAD>
2022-06-24 12:04:38 +0530

    update

--- firstpj/src/firstpj/FirstPj.java
+++ firstpj/src/firstpj/FirstPj.java
@@ -5,6 +5,8 @@ public class FirstPj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome to java projects");
+System.out.println("************************");
+
 	}
 
 }
